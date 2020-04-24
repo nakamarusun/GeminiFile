67c06597a7865f85a11d0e01597abe326aa1a7eb
nakamarusun <48478366+nakamarusun@users.noreply.github.com>
2020-04-24 10:33:49 +0700

    Added comments and todo for CLI project

--- com/geminifile/Driver.java
+++ com/geminifile/Driver.java
@@ -1,8 +1,13 @@
 package com.geminifile;
 
+/*
+By Jason Christian @ https://www.github.com/nakamarusun
+*/
+
 public class Driver {
     public static void main(String[] args) {
 
+        // TODO: complete --help and use regex (or .contains) for CLI command recognition. Make into a function
         if (args.length != 0) {
             switch (args[0]) {
                 case "--help":
