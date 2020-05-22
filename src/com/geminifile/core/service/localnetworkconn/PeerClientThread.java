dbc977005ac5bc854c631054ee2f6b426f9745f8
Jason Christian H <nakamarujc@gmail.com>
2020-05-22 22:46:37 +0700

    Implemented PeerClientThreaduntil main loop

--- src/com/geminifile/core/service/localnetworkconn/PeerClientThread.java
+++ src/com/geminifile/core/service/localnetworkconn/PeerClientThread.java
@@ -1,11 +1,15 @@
 package com.geminifile.core.service.localnetworkconn;
 
+import com.geminifile.core.service.Service;
+import com.geminifile.core.socketmsg.MsgType;
 import com.geminifile.core.socketmsg.msgwrapper.*;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+
+// This thread can also accept ping queries.
 
 public class PeerClientThread implements Runnable {
 
@@ -23,9 +27,24 @@ public class PeerClientThread implements Runnable {
             ObjectInputStream localObjectIn = new ObjectInputStream(sock.getInputStream());
 
             try {
-                // Accept query from the peer.
+                // Accept query from the peer. Whether this is a query or not.
                 MsgIdentification inQuery = (MsgIdentification)localObjectIn.readObject();
+                // Send self id as CONNQUERY accept
+                localObjectOut.writeObject(new MsgIdentification("reply", MsgType.CONNQUERY, Service.getMyNode()));
+                // Accept an OK request
+                MsgWrapper okMsg = (MsgWrapper)localObjectIn.readObject();
 
+                // If connection is accepted and message is right then add to peerTable
+                if (okMsg.getType() == MsgType.CONNACCEPT && okMsg.getContent().equals("OKClient")) {
+                    PeerCommunicatorManager.addPeerTable(inQuery.getSelfNode());
+                } else {
+                    // Exit thread
+                    return;
+                }
+
+                while (true) {
+                    // Main message manager loop
+                }
 
             } catch (ClassNotFoundException e) {
                 System.out.println("Class deserialization error");
